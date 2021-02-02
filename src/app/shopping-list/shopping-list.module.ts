b65eeb49eef3cbdbe8ac98c1daf963030cf7f727
FiguraOcho <figura.ocho@gmail.com>
2021-02-02 22:35:18 +0100

    fin del primer dia

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
 import { ShoppingListComponent } from "./shopping-list.component";
@@ -8,7 +8,7 @@ import { ShoppingListService } from "./shopping-list.service";
 
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingEditComponent],
-  imports: [CommonModule, FormsModule, ShoppingListRoutingModule],
-  providers: [ShoppingListService]
+  imports: [FormsModule, ShoppingListRoutingModule, SharedModule],
+  providers: [ShoppingListService],
 })
 export class ShoppingListModule {}
